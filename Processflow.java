23110c84e32f3dba232303f79e179fa420de526b
potrandrei <43883781+potrandrei@users.noreply.github.com>
2019-01-31 20:43:23 +0200

    Update Processflow.java

--- Processflow.java
+++ Processflow.java
@@ -4,16 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.PrintWriter;
+import java.awt.*;
+import java.util.Scanner;
 
 public class Processflow
 {
     public static void main( String[] args ) throws Exception
     {
-        FileReader fileReader = new FileReader("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\Log\\Valori electrice.txt");
+        FileReader fileReader = new FileReader("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\Valori electrice.txt");
         BufferedReader reader = new BufferedReader(fileReader);
 
-        String text = "";                                         // objekt that retains the text from the file readed.
-        String line = reader.readLine();                          // read the file from begining to end.
+        String text = "";                              // objekt that retains the electrical values fromthe specified file.
+        String line = reader.readLine();               // read the file from begining to end.
 
         while (line != null){
             text = text + line;
@@ -21,9 +23,20 @@ public class Processflow
         }
 
 // Write the data stored in the buffer.
-        PrintWriter output = new PrintWriter("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\Log\\ProcesFlow.txt");
+        PrintWriter output = new PrintWriter("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\ProcesFlow.txt");
         output.println(text);
+
+
+// Initiate the request of the values from the barcode.
+        String barcodescanner;
+
+        Scanner barcode = new Scanner(System.in);
+        System.out.println("Please scan the bar code ... ");
+        barcodescanner = barcode.next();
+        output.print(barcodescanner);
+
         output.close();
+
 
 
     }
