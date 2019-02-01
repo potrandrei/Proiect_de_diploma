18be5596c131bef2034f0e03287f430aa47f6958
potrandrei <43883781+potrandrei@users.noreply.github.com>
2019-02-01 17:58:02 +0200

    Update Processflow.java

--- Processflow.java
+++ Processflow.java
@@ -11,32 +11,31 @@ public class Processflow
 {
     public static void main( String[] args ) throws Exception
     {
-        FileReader fileReader = new FileReader("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\Valori electrice.txt");
-        BufferedReader reader = new BufferedReader(fileReader);
+            FileReader fileReader = new FileReader("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\Valori electrice.txt");
+            BufferedReader reader = new BufferedReader(fileReader);
 
-        String text = "";                              // objekt that retains the electrical values fromthe specified file.
-        String line = reader.readLine();               // read the file from begining to end.
+            String text = "";                              // objekt that retains the electrical values fromthe specified file.
+            String line = reader.readLine();               // read the file from begining to end.
 
-        while (line != null){
-            text = text +" " + line;
-            line = reader.readLine();
-        }
+            while (line != null) {
+                text = text + " " + line;
+                line = reader.readLine();
+            }
 
 // Write the data stored in the buffer.
-        PrintWriter output = new PrintWriter("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\ProcesFlow.txt");
-        output.print(text);
+            PrintWriter output = new PrintWriter("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\ProcesFlow.txt");
+            output.print(text);
+        System.out.println("1. Valorice electrice au fost actualizate");
 
 // Initiate the request of the values from the barcode.
-        String barcodescanner;
+            String barcodescanner;
 
-        Scanner barcode = new Scanner(System.in);
-        System.out.println("Please scan the bar code ... ");
-        barcodescanner = barcode.next();
-        output.print(" " + barcodescanner);
+            Scanner barcode = new Scanner(System.in);
+            System.out.println("2. Scanati va rog codul de bare ... ");
+            barcodescanner = barcode.next();
+            output.print(" " + barcodescanner);
 
-        output.close();
-        System.out.println("The file was updated with the new values");
-
-
+            output.close();
+        System.out.println("3. Fisierul a fost actualizat ...");
     }
 }
