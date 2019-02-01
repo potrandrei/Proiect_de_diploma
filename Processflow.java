b6f0cac9a4d8093ce7a68c5964b8088d3f6562b4
potrandrei <43883781+potrandrei@users.noreply.github.com>
2019-02-02 00:01:44 +0200

    Update Processflow.java

--- Processflow.java
+++ Processflow.java
@@ -7,35 +7,63 @@ import java.io.PrintWriter;
 import java.awt.*;
 import java.util.Scanner;
 
+import jxl.Workbook;
+import jxl.write.DateTime;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Processflow
 {
     public static void main( String[] args ) throws Exception
     {
+//Read the electrical values from the Mltimeter file.
             FileReader fileReader = new FileReader("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\Valori electrice.txt");
             BufferedReader reader = new BufferedReader(fileReader);
 
-            String text = "";                              // objekt that retains the electrical values fromthe specified file.
-            String line = reader.readLine();               // read the file from begining to end.
+            String text = "";
+            String line = reader.readLine();
 
             while (line != null) {
                 text = text + " " + line;
                 line = reader.readLine();
             }
 
-// Write the data stored in the buffer.
+// Write the data stored in the Procesflow.txt.
             PrintWriter output = new PrintWriter("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\src\\main\\Log\\ProcesFlow.txt");
             output.print(text);
         System.out.println("1. Valorice electrice au fost actualizate");
+        TimeUnit.SECONDS.sleep(1);
 
 // Initiate the request of the values from the barcode.
             String barcodescanner;
-
             Scanner barcode = new Scanner(System.in);
             System.out.println("2. Scanati va rog codul de bare ... ");
             barcodescanner = barcode.next();
             output.print(" " + barcodescanner);
 
+            TimeUnit.SECONDS.sleep(1);
             output.close();
         System.out.println("3. Fisierul a fost actualizat ...");
+
+// Create a excel file.
+
+        File f = new File("D:\\Programare\\Programe\\IntelliJ\\Proiect de Diploma\\RezultatValori.xls");
+
+            WritableWorkbook myexcel = Workbook.createWorkbook(f);
+            WritableSheet mysheet = myexcel.createSheet("mysheet",0);
+        Label l = new Label(0,0,"data 1");
+
+        mysheet.addCell(new Label(0, 0, "Numar intern"));
+        mysheet.addCell(new Label(1, 0, "Rezistenta masurata"));
+        mysheet.addCell(new Label(2, 0, "Data masuratorii"));
+
+
+        myexcel.write();
+        myexcel.close();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("A fost creat fisirul excel cu datele masuratorilor.");
     }
 }
